72b19c78e90aef9acc8565f59c43470f5abcc477
深水 <jiandinglixiang@qq.com>
2019-04-02 18:33:18 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path')
 var router = express.Router();
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/*', function (req, res, next) {
   res.sendFile('index.html', { root: process.cwd() + '/App/' })
 });
 module.exports = router;
